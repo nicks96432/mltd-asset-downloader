e565be80534ab57b94fdcee274a9f4081d37bb37
我可能很廢 <nicks96432@gmail.com>
2024-01-09 12:49:08 +0800

    :art: Fmt

--- extract/src/class/mod.rs
+++ extract/src/class/mod.rs
@@ -1,8 +1,8 @@
 //! This module contains methods for extracting Unity objects, because
 //! rabex doesn't provide any yet.
 
-pub mod text_asset;
 pub mod asset_bundle;
 pub mod mesh;
 pub mod sprite;
+pub mod text_asset;
 pub mod texture_2d;
